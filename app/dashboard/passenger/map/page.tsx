20a9ac27e7d295260fdb1aa937e1b65eb3998d00
Macharia11 <stevieskaka3096@gmail.com>
2026-02-08 22:36:15 +0300

    woah

--- app/dashboard/passenger/map/page.tsx
+++ app/dashboard/passenger/map/page.tsx
@@ -4,8 +4,16 @@ import { LiveMapView } from "@/components/map/live-map-view"
 
 export default function PassengerMapPage() {
   return (
-    <div className="-m-4 lg:-m-6">
+    <div className="-m-4 lg:-m-6 h-[calc(100vh-64px)]"> {/* Add explicit height */}
       <LiveMapView />
     </div>
   )
 }
+
+// export default function PassengerMapPage() {
+//   return (
+//     <div className="-m-4 lg:-m-6">
+//       <LiveMapView />
+//     </div>
+//   )
+// }
